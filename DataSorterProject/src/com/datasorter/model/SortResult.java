b2edb40ea88df194e8594df9e2fadd980f0572a7
Mihir Chakma <mihirchakmaths@gmail.com>
2025-10-29 15:17:02 +0530

    Initial project setup with package structure

--- DataSorterProject/src/com/datasorter/model/SortResult.java
+++ DataSorterProject/src/com/datasorter/model/SortResult.java
@@ -1,4 +1,16 @@
 package com.datasorter.model;
 
+/**
+ * A simple class to hold the result of a sort operation,
+ * including the sorted array and the number of steps (comparisons).
+ */
+
 public class SortResult {
+    public int[] sortedArray;
+    public long steps;
+
+    public SortResult(int[] sortedArray, long steps) {
+        this.sortedArray = sortedArray;
+        this.steps = steps;
+    }
 }
