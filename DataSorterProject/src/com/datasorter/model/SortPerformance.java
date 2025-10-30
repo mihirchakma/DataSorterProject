9f28b23a1a43dc076018c6ad3061267ada6075af
Mihir Chakma <mihirchakmaths@gmail.com>
2025-10-30 20:10:39 +0530

    feat: Implement Sort Performance class

--- DataSorterProject/src/com/datasorter/model/SortPerformance.java
+++ DataSorterProject/src/com/datasorter/model/SortPerformance.java
@@ -1,4 +1,19 @@
 package com.datasorter.model;
 
+/**
+ * Holds the complete results of a single sort test.
+ */
+
 public class SortPerformance {
+    public String algoName;
+    public double execTime;
+    public long steps;
+    public int[] sortedArray;
+
+    public SortPerformance(String algoName, double execTime, long steps, int[] sortedArray) {
+        this.algoName = algoName;
+        this.execTime = execTime;
+        this.steps = steps;
+        this.sortedArray = sortedArray;
+    }
 }
