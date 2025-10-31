6193980c7f06cdf19a475abb0c1e1b6a4bea75db
“[firstname <ruchiravishvajith6@gmail.com>
2025-10-31 17:43:35 +0530

    created for loop

--- DataSorterProject/src/com/datasorter/algorithms/BubbleSort.java
+++ DataSorterProject/src/com/datasorter/algorithms/BubbleSort.java
@@ -8,5 +8,24 @@ public class BubbleSort {
         int n = arr.length;
         boolean swapped;
 
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                steps++;
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+        return new SortResult(arr, steps);
+    }
+}
+
 public class BubbleSort {
 }
