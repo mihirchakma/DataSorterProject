86e2034011ca6d9c37e9ec29660df9083657fc8f
“[firstname <ruchiravishvajith6@gmail.com>
2025-10-31 17:34:46 +0530

    created public class Bubble sort

--- DataSorterProject/src/com/datasorter/algorithms/BubbleSort.java
+++ DataSorterProject/src/com/datasorter/algorithms/BubbleSort.java
@@ -3,4 +3,10 @@ package com.datasorter.algorithms;
 import com.datasorter.model.SortResult;
 
 public class BubbleSort {
+    public static SortResult sort(int[] arr) {
+        long steps = 0;
+        int n = arr.length;
+        boolean swapped;
+
+public class BubbleSort {
 }
